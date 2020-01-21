cf48425420749a1254c741137df9c179ef917160
Prkizir <A01020382@itesm.mx>
2020-01-21 17:57:00 -0600

    Tutorials first commit

---
+++ ROSdev/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/prkizir/Desktop/ITESM/10mo/AppliedRobotics/ROSdev/src
+# Build directory: /home/prkizir/Desktop/ITESM/10mo/AppliedRobotics/ROSdev/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("beginner_tutorials")
